88ab79bc17a350c1a38ce0494041f153f3c788f9
soumil <soumilnegi154@gmail.com>
2025-09-26 18:21:04 +0530

    question 5

--- task1/question5.py
+++ task1/question5.py
@@ -1,6 +1,12 @@
-def fibonacci(num):
-    if num==2:
-        return [1, 1]
-    return fibonacci(num-1).append(fibonacci(num-1)[-1]+fibonacci(num-1)[-2])
+#Find the fabonacci of a given number using recursion.
 
-print(fibonacci(5))
+def rfib(n):
+    if (n==1):
+        return 0
+    elif (n==2):
+        return 1
+    return rfib(n-1)+rfib(n-2)
+
+terms=5
+for i in range(1, terms+1):
+    print(rfib(i), end=" ")
